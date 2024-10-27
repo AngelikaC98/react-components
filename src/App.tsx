ac951a92926a346a9796c025d3ee28e7fc2fe0d9
Angelika Ciuchcińska <angelikaciuchcinska@Angelikas-MacBook-Air.local>
2024-10-27 19:41:07 +0000

    I did basic webcard for my first task using React

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+
+import {Card} from "./components/card"
+import "./App.css"
+
+
+function App() {
+  
+
+  return (
+    <div className="Container">
+    <Card/>
+    <Card/>
+    <Card/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/card.tsx
@@ -0,0 +1,19 @@
+
+
+import "./card.css"
+
+export const Card = () => {
+    const nextPage = "https://www.americanhippotherapyassociation.org/what-is-hippotherapy"
+    return (
+        <div className="border">
+            <img className="image" src="../../public/hippotherapy.jpg" />
+            <h2 className="text"> Title</h2>
+            <p className="paragraph"> Here will be some text about card.</p>
+            <a href={nextPage}>
+                <button className="button">
+                    Read more
+                </button>
+            </a>
+        </div>
+    )
+}
