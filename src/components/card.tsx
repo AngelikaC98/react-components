27ace19dd19b579b41f3b08941db25f818ac233d
Angelika Ciuchcińska <angelikaciuchcinska@Angelikas-MacBook-Air.local>
2024-10-27 19:58:41 +0000

    fixed photo

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,7 +6,7 @@ export const Card = () => {
     const nextPage = "https://www.americanhippotherapyassociation.org/what-is-hippotherapy"
     return (
         <div className="border">
-            <img className="image" src="./hippotherapy.jpg" />
+            <img className="image" src="https://github.com/AngelikaC98/react-components/blob/main/public/hippotherapy.jpg?raw=true" />
             <h2 className="text"> Title</h2>
             <p className="paragraph"> Here will be some text about card.</p>
             <a href={nextPage}>
